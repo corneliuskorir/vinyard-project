3d53b8d4ffb72e7f5f7cad230081016691e1d5c0
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-08-14 15:22:26 +0300

    dashboard summary layout setup

--- src/pages/dashboard/DashboardSummary.jsx
+++ src/pages/dashboard/DashboardSummary.jsx
@@ -1,8 +1,17 @@
+import styles from "./DashboardSummary.module.css";
+
 function DashboardSummary() {
   return (
-    <div>
-      <h1> Welcome back Admin</h1>
-      <p>This is a summary of what to expect</p>
+    <div className={styles.summary}>
+      <div className={styles.title}>
+        <h1> Welcome back Admin</h1>
+        <p>This is a summary of what to expect</p>
+      </div>
+      <div className={styles.content}>
+        <div className={`${styles.summaryBox} shop`}>Shop summary</div>
+        <div className={`${styles.summaryBox} visits`}>Visits summary </div>
+        <div className={`${styles.summaryBox} events`}>Events summary </div>
+      </div>
     </div>
   );
 }
