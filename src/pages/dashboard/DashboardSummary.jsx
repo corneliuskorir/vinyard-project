181b6a058f4cf489d6274cd93df699815b1d7e7b
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-08-14 15:29:32 +0300

    dashboard summary layout changes

--- src/pages/dashboard/DashboardSummary.jsx
+++ src/pages/dashboard/DashboardSummary.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./DashboardSummary.module.css";
 
 function DashboardSummary() {
@@ -8,9 +9,18 @@ function DashboardSummary() {
         <p>This is a summary of what to expect</p>
       </div>
       <div className={styles.content}>
-        <div className={`${styles.summaryBox} shop`}>Shop summary</div>
-        <div className={`${styles.summaryBox} visits`}>Visits summary </div>
-        <div className={`${styles.summaryBox} events`}>Events summary </div>
+        <div className={`${styles.summaryBox} shop`}>
+          <p> Shop summary </p>
+          <Link to="/dashboard/shop">View more</Link>
+        </div>
+        <div className={`${styles.summaryBox} visits`}>
+          <p> Visits summary </p>
+          <Link to="/dashboard/visits">View more</Link>
+        </div>
+        <div className={`${styles.summaryBox} events`}>
+          <p>Events summary</p>
+          <Link to="/dashboard/events">View more</Link>
+        </div>
       </div>
     </div>
   );
