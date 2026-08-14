7e407181f9e93f5535c0e5047ce22c787cbd9d0a
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-08-14 14:37:35 +0300

    dashboard main routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashboardLayout from "./pages/dashboard/DashboardLayout";
 import DashboardSummary from "./pages/dashboard/DashboardSummary";
+import DashboardShop from "./pages/dashboard/DashboardShop";
+import DashboardEvents from "./pages/dashboard/DashboardEvents";
+import DashboardVisits from "./pages/dashboard/DashboardVisits";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<DashboardLayout />}>
             <Route index element={<DashboardSummary />} />
+            <Route path="Shop" element={<DashboardShop />}></Route>
+            <Route path="events" element={<DashboardEvents />}></Route>
+            <Route path="visits" element={<DashboardVisits />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/dashboard/DashboardEvents.jsx
@@ -0,0 +1,9 @@
+function DashboardEvents() {
+  return (
+    <div>
+      <h1> Events go here bruh!</h1>
+    </div>
+  );
+}
+
+export default DashboardEvents;
