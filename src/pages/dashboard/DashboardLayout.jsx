9e54d598b12691b7db9da4d09ed6f61afd7c1d80
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-08-14 14:05:26 +0300

    dashboard layout and navlinks

--- src/pages/dashboard/DashboardLayout.jsx
+++ src/pages/dashboard/DashboardLayout.jsx
@@ -1,5 +1,27 @@
+import { NavLink, Outlet } from "react-router-dom";
+import styles from "./DashboardLayout.module.css";
+
 function DashboardLayout() {
-  return <h1> Dashboard baby !</h1>;
+  return (
+    <div className={styles.dashboard}>
+      <div className={styles.navigation}>
+        <div className={styles.header}>
+          <p>Ambrosia</p>
+          <h1>Dashboard</h1>
+        </div>
+        <div>
+          <nav>
+            <NavLink to="/dashboard/shop">Shop</NavLink>
+            <NavLink to="/dashboard/visits">Visits</NavLink>
+            <NavLink to="/dashboard/events">Events</NavLink>
+          </nav>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default DashboardLayout;
