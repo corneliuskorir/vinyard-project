91403f6d8e20a4f733cdd821eddbbf06616e2fb4
davidnjagih-png <david.njagih@student.moringaschool.com>
2026-08-15 20:25:32 +0300

    footer contact added and css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2026 Your Company. All rights reserved.</p>
+      <div className="footer-left">
+        <p>&copy; 2026 Your Company. All rights reserved.</p>
+      </div>
+      <div className="footer-right">
+        <p>📧 Email: info@suncrestvalley.com | 📞 Phone: +254 70* *** *56</p>
+      </div>
     </footer>
   );
 }
