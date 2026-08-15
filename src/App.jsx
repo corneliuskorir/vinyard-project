6faf3a578f114f251ca77da5f6e5eaa84dc8507c
Harry Salim <salimharry96@gmail.com>
2026-08-16 01:19:11 +0300

    Implemented shop products and category

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LandingPage from "./pages/LandingPage";
+import Shop from "./pages/Shop";
 
 import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState({
+    wines: [],
+    cheeses: [],
+  });
+
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/store/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("GET DATA:", data.products);
+
+        setProducts(data.products);
+      });
+  }, []);
   return (
     <Router>
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/shop"
+            element={
+              <Shop products={products} setShoppingCart={setShoppingCart} />
+            }
+          />
         </Routes>
       </main>
       <Footer />
