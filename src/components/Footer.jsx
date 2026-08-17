e666cdc005719cb912fe3279bbc3c303d09f8153
davidnjagih-png <david.njagih@student.moringaschool.com>
2026-08-17 12:43:24 +0300

    footer code and css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,38 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-left">
-        <p>&copy; 2026 Your Company. All rights reserved.</p>
+        <p>
+          📧 Email:{" "}
+          <a href="mailto:info@suncrestvalley.com">info@suncrestvalley.com</a>
+        </p>
+        <p>
+          📞 Phone: <a href="tel:+254702485756">+254 702 485 756</a>
+        </p>
+        <p>&copy; 2026 Suncrest Valley. All rights reserved.</p>
       </div>
       <div className="footer-right">
-        <p>📧 Email: info@suncrestvalley.com | 📞 Phone: +254 702 485 756</p>
+        <nav className="footer-nav">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/shop" className="nav-link">
+            Shop
+          </NavLink>
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+          <NavLink to="/events" className="nav-link">
+            Events
+          </NavLink>
+          <NavLink to="/dashboard" className="nav-link">
+            Dashboard
+          </NavLink>
+        </nav>
       </div>
     </footer>
   );
